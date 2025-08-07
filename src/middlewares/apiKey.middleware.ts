a3d399e620355cbf02cc3ce3414c3c5bafda3c30
Gajendra Rao <gajendrapawar69@gmail.com>
2025-08-07 16:58:01 +0530

    name changed to LEADS_API_KEY

--- src/middlewares/apiKey.middleware.ts
+++ src/middlewares/apiKey.middleware.ts
@@ -1,17 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 
 // You should store this securely in a .env file
-const STATIC_API_KEY = process.env.STATIC_API_KEY;
+const LEADS_API_KEY = process.env.LEADS_API_KEY;
 
 export const verifyApiKey = (req: Request, res: Response, next: NextFunction) => {
-const apiKey = req.header("x-api-key");
+const apiKey = req.header("LEADS_API_KEY");
 
-if (!STATIC_API_KEY) {
-console.error("STATIC_API_KEY is not defined in environment variables.");
+if (!LEADS_API_KEY) {
+console.error("LEADS_API_KEY is not defined in environment variables.");
 return res.status(500).json({ error: "Internal Server Error" });
 }
 
-if (!apiKey || apiKey !== STATIC_API_KEY) {
+if (!apiKey || apiKey !== LEADS_API_KEY) {
 return res.status(401).json({ error: "Unauthorized: Invalid or missing API key" });
 }
 
